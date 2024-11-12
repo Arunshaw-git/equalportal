b8485ac3b0976dfcf514ae7c11b7045f24579d98
Arunshaw-git <128923649+Arunshaw-git@users.noreply.github.com>
2024-11-13 00:21:44 +0530

    Now the files will be directly send to cloudinary 2

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,10 +1,11 @@
 // backend/config/multer.js
 const multer = require('multer');
 const path = require("path");
+const cloudinary = require('cloudinary').v2; // Import Cloudinary
 
 // Set up multer storage (to store temporary file paths)
 const storage = multer.diskStorage({
-  loudinary: cloudinary,  // Cloudinary instance
+  cloudinary: cloudinary,  // Cloudinary instance
   params: {
     folder: 'equalPortal/posts',  // Cloudinary folder
   },
