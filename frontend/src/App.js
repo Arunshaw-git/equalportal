a090bd19b813b8bbcfd33f99ccd7970f03072889
Arunshaw-git <128923649+Arunshaw-git@users.noreply.github.com>
2025-02-07 14:50:54 +0530

    fixed the CreateUser not found issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Homepage from './myComponents/Homepage'; // Example import of your homepage
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './myComponents/Login'; // Example import of your homepage
-import CreatePost from './myComponents/CreatePost'; // Example import of your homepage
-import Footer from './myComponents/Footer'; // Import your Footer component
+import CreatePost from './myComponents/CreatePost'; 
+import Footer from './myComponents/Footer'; 
 import CreateUser from './myComponents/CreateUser';
 
 
