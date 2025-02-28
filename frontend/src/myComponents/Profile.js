27c11350417ccbb773d2510159ec29faa44d02c8
Arunshaw-git <128923649+Arunshaw-git@users.noreply.github.com>
2025-02-28 20:17:02 +0530

    fixed the react hook missing dependecy:'navigate'

--- frontend/src/myComponents/Profile.js
+++ frontend/src/myComponents/Profile.js
@@ -3,16 +3,16 @@ import { useNavigate, Link } from "react-router-dom";
 import Logout from "./Logout";
 const Profile = () => {
   const [user, setUser] = useState([]);
-  const [error, setError] = useState(null);
   const navigate = useNavigate();
   const apiUrl = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     const fetchProfile = async () => {
       const token = localStorage.getItem("token");
+
       if (!token) {
         console.log("Token not provided while fetching  ");
-        navigate("/login");
+        navigate("/login"); // Redirect to login
         return;
       }
 
@@ -25,20 +25,17 @@ const Profile = () => {
           },
         });
 
-        if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(errorData.error || "Failed to fetch user's profile");
-        }
+        if (!response.ok) throw new Error( "Failed to fetch user's profile");
+        
         const data = await response.json();
         console.log(data);
         setUser(data);
       } catch (e) {
-        setError(e.message);
         console.log("Profile Error:", e);
       }
     };
     fetchProfile();
-  }, [apiUrl]);
+  }, [apiUrl,navigate]);
 
   return (
     <>
