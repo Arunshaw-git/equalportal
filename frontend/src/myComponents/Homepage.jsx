26cf007a4f5b8cdee3fc4ee3142ce8367c102d61
Arunshaw-git <128923649+Arunshaw-git@users.noreply.github.com>
2025-08-15 17:15:48 +0530

    chekc

--- frontend/src/myComponents/Homepage.jsx
+++ frontend/src/myComponents/Homepage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Homepage.css";
--- frontend/src/myComponents/Profile.js
+++ frontend/src/myComponents/Profile.js
@@ -77,7 +77,7 @@ const Profile = () => {
       }
     };
     fetchProfile();
-  }, [apiUrl]);
+  }, [apiUrl,navigate]);
 
   return (
     <>
